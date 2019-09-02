9b9e15f9a06ea707f5c62458b5c55ceb572e9e82
Frank Ruiz <frankruiz49@gmail.com>
2019-09-02 16:11:18 -0700

    added messaging functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
   end
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
      flash[:success] = "You have successfully logged in"
-     redirect to root_path
+     redirect_to root_path
 
     else
       flash.now[:error] = "There was something wrong with your login information"
@@ -20,5 +21,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+  
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end 
